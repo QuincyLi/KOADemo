61bd3b4e497e1b0825b79a2027a56ea01adc9351
李权 <466217509@qq.com>
2018-08-10 16:36:03 +0800

    重置依赖

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import appState from './Observables/appState';
 
-import Observable from './observable';
+@observer
+class TimerView extends React.Component {
+  componentWillReceiveProps(){
+    console.log(11);
+  }
 
-ReactDOM.render(<Observable />, document.getElementById("app"));
+    render() {
+        return (<button onClick={this.onReset}>
+                Seconds passed: {this.props.appState.timer}
+            </button>);
+    }
+
+    onReset = () => {
+        this.props.appState.resetTimer();
+    }
+};
+
+ReactDOM.render(<TimerView appState={appState} />, document.getElementById('app'));
