0371ed0497bda256a402782244e0e0ed193550ea
李权 <466217509@qq.com>
2018-08-10 09:06:08 +0800

    新增index模版文件

--- sever/routes/index.js
+++ sever/routes/index.js
@@ -1,9 +1,11 @@
+import fs from 'fs';
 import Router from 'koa-router';
 
 const router = new Router();
 
 router.get('/', (ctx) => {
-  ctx.body = 'hello world'
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./src/index.html');
 });
 
 export default router;
